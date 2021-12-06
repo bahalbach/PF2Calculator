522e2f091eaae70c9520972dcf7765fd8e3d2a89
bahalbach <bahalbach@gmail.com>
2021-12-06 22:02:08 +0800

    Was saving graphs with wrong names...

--- src/tests/PF2App.test.tsx
+++ src/tests/PF2App.test.tsx
@@ -1,3 +1,4 @@
+import { getByText, screen } from "@testing-library/dom";
 import React from "react";
 import PF2App from "../App/PF2App";
 import { render } from "./test-utils";
@@ -5,5 +6,29 @@ import { render } from "./test-utils";
 describe("PF2App", () => {
   test("renders", () => {
     render(<PF2App />);
+    // screen.querySelector(
+    //   "#root > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(3) > div > div > div > svg:nth-child(3) > g:nth-child(4) > g:nth-child(1) > g > g > g:nth-child(1) > rect"
+    // );
+  });
+
+  test("renders Routines correctly", () => {
+    const { container } = render(<PF2App />);
+
+    // the first list is the list of routines
+    let routines = screen.getAllByRole("list")[0];
+
+    // 7 routines loaded by default
+    expect(routines.children.length).toBe(7);
+
+    //screen.debug(routines);
+    // the first loaded routine is 'baseline'
+    expect(
+      getByText(routines.children[0] as HTMLElement, "Baseline")
+    ).toBeInTheDocument();
+
+    // let baselineOnGraph = container.querySelector(
+    //   "#root > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(3) > div > div > div > svg:nth-child(3) > g:nth-child(4) > g:nth-child(1) > g > g > g:nth-child(1) > rect"
+    // );
+    // screen.debug(baselineOnGraph);
   });
 });
