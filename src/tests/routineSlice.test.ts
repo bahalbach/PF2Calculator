afe44b14fb0885b831be876697d14db8161546ba
bahalbach <bahalbach@gmail.com>
2021-12-10 20:52:22 +0800

    set up testing routine slice

---
+++ src/tests/routineSlice.test.ts
@@ -0,0 +1,29 @@
+import {
+  setRoutine,
+  setActivityPath,
+  setNewActivityParent,
+  routineAdded,
+  routineUpdated,
+  routineCreated,
+  routineRemoved,
+  activityPathAdded,
+  activityPathUpdated,
+  emptyActivityPathCreated,
+  activityPathCreated,
+  activityPathContinued,
+  activityPathRemoved,
+  damageAdded,
+  damageUpdated,
+  damageCreated,
+  damageRemoved,
+  effectAdded,
+  effectUpdated,
+  effectCreated,
+  effectRemoved,
+  importRoutine,
+} from "../Routines/RoutineSlice/routineSlice";
+import reducer from "../Routines/RoutineSlice/routineSlice";
+
+describe("routineSlice reducer", () => {
+  test("activityPathCreated", () => {});
+});
