efb28ac29cc348c2968cf38b5b015f45b31a92d1
Ben Halbach <bahalbach@gmail.com>
2024-02-17 19:55:40 +0800

    update github pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/PF2Calculator/',
   plugins: [react()],
   test: {
     globals: true,
